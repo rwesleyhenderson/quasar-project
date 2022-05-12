7ef6545477f286db92097eae7095d2f7d901de80
Wes Henderson <whenderson@nsgdv.com>
2022-05-12 15:08:58 -0500

    first build

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,71 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-card class="my-card" style="width: 600px">
+      <q-card-section>
+        <div class="text-h4">To-Do List</div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="row justify-center">
+          <div class="col-12 q-pb-md">
+            <q-input
+              v-model="todoTask"
+              label="Input Task"
+              @keyup.enter="addItem"
+            >
+              <template v-slot:after>
+                <q-btn round dense flat icon="send" @click="addItem" />
+              </template>
+            </q-input>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-separator />
+      <q-card-section v-if="todoList.length">
+        <div
+          class="row justify-center"
+          v-for="(item, index) in todoList"
+          :key="index"
+        >
+          <div class="col-8 q-py-md">
+            {{ item.value }}
+          </div>
+          <div class="col-auto q-py-md">
+            <q-btn round dense flat icon="done" @click="removeItem(index)" />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section v-else>
+        <div class="row justify-center">
+          <div class="col-12">
+            <div class="text-h6">No Items in the List</div>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  data() {
+    return {
+      todoTask: "",
+      todoList: [{ value: "Create an app" }, { value: "Build an app" }],
+    };
+  },
+  methods: {
+    addItem() {
+      this.todoList.push({ value: this.todoTask });
+    },
+    removeItem(index) {
+      this.todoList.splice(index, 1);
+    },
+  },
+});
 </script>
